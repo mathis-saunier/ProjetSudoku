f35a030f2ac300ec462ca8fa4b041511302c5f28
Mister-S-moi <ninjago78@orange.fr>
2024-10-21 14:48:06 +0200

    chore: définition des fonctions de SudokuBrutForce

---
+++ src/resolutionBrutForce.py
@@ -0,0 +1,11 @@
+import sudoku as s
+
+class SudokuBrutForce(s.Sudoku):
+
+    def __init__(self, taille: int = 9) -> None:
+        super().__init__(taille)
+
+    def resoudreSudoku(self) -> bool:
+        pass
+
+    
